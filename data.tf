56ccdffaa114b07c24b093a04680fbce494b1b10
Kesava Kadiyala <kesavakadiyala@gmail.com>
2022-03-04 21:56:07 +0530

    Adding Data for frontend remote state

--- data.tf
+++ data.tf
@@ -8,6 +8,16 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
+data "terraform_remote_state" "frontend" {
+  backend = "s3"
+
+  config = {
+    bucket      = var.bucket
+    key         = "frontend/${var.ENV}/terraform.tfstate"
+    region      = "us-east-1"
+  }
+}
+
 data "aws_ami" "ami" {
   owners           = ["342998638422"]
   most_recent      = true
